b7ab9d78723da91b3aa9a5da4b94180743eac4ef
drkaine <duretkevin@live.fr>
2024-05-25 11:28:37 +0000

    [ACT1] feat: add a seed
    Add a seed for the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,8 @@ declare(strict_types = 1);
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Assignment;
+use App\Models\Candidate;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,11 +15,7 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run(): void
 	{
-		// User::factory(10)->create();
-
-		User::factory()->create([
-			'name' => 'Test User',
-			'email' => 'test@example.com',
-		]);
+		Candidate::factory(20)->create();
+		Assignment::factory(50)->create();
 	}
 }
