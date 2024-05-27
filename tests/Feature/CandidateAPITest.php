5d279a80fac77489c9537c49cbb7637f8cd54135
drkaine <duretkevin@live.fr>
2024-05-27 08:03:59 +0000

    [ACT-01] feat: refactoring suppress duplication
    Test see all candidates is include in the test see all candidates and ther assigments

--- tests/Feature/CandidateAPITest.php
+++ tests/Feature/CandidateAPITest.php
@@ -7,15 +7,6 @@ use App\Models\Candidate;
 
 uses(Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-test('See all the candidates ', function (): void {
-	Candidate::factory()->count(5)->create();
-
-	$response = $this->getJson('/api/candidates');
-
-	$response->assertStatus(200);
-	$response->assertJsonCount(5);
-});
-
 test('See all the candidates and their assignments ', function (): void {
 	Candidate::factory()->count(5)->create();
 	Assignment::factory()->count(5)->create(
