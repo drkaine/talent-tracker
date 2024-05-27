eed9b7d09bea432b5b2c0d55ca1f8fd23f00771b
drkaine <duretkevin@live.fr>
2024-05-27 05:51:48 +0000

    [ACT-01] feat: refactoring test
    Transform deplicate test with use of dataset

--- tests/Feature/ModelTest.php
+++ tests/Feature/ModelTest.php
@@ -7,35 +7,34 @@ use App\Models\Candidate;
 
 uses(Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-test('Assignment ', function (): void {
-	$assignmentData = [
-		'start_date' => '01/01/2024',
-		'end_date' => '01/01/2025',
-		'title' => 'Mission de test',
-		'candidate_id' => 1,
+dataset('data for test asssignment model', function () {
+	return [
+		[
+			[
+				'start_date' => '01/01/2024',
+				'end_date' => '01/01/2025',
+				'title' => 'Mission de test',
+				'candidate_id' => 1,
+			],
+		],
+		[
+			[
+				'start_date' => '01/01/2024',
+				'end_date' => '01/01/2025',
+				'title' => 'Mission de test',
+				'candidate_id' => null,
+			],
+		],
 	];
+});
 
+test('Assignment ', function (array $assignmentData): void {
 	$assignment = new Assignment;
 
 	$assignment->factory()->create($assignmentData);
 
 	$this->assertDatabaseHas('assignments', $assignmentData);
-});
-
-test('Assignment with candidate_id at null', function (): void {
-	$assignmentData = [
-		'start_date' => '01/01/2024',
-		'end_date' => '01/01/2025',
-		'title' => 'Mission de test',
-		'candidate_id' => null,
-	];
-
-	$assignment = new Assignment;
-
-	$assignment->factory()->create($assignmentData);
-
-	$this->assertDatabaseHas('assignments', $assignmentData);
-});
+})->with('data for test asssignment model');
 
 test('Candidate ', function (): void {
 	$candidateData = [
